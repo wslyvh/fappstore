73a5f04483ba6867f527232f0120d03ce4156b7e
wslyvh <wslyvh@users.noreply.github.com>
2025-05-13 08:34:18 +0200

    add frame fix

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -35,7 +35,7 @@ export function Layout(props: PropsWithChildren) {
                 console.log("Adding frame..");
                 await sdk.actions.addFrame();
               } catch (error) {
-                console.error(error);
+                console.error("Error adding frame", error);
               }
             }}
           >
